dc2c4c328ae2e8e86a8efbf920c70debee7b1379
Abhijith Rajeev Menon <52545355+RudCodera8@users.noreply.github.com>
2020-11-07 20:20:48 +0530

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -1,6 +1,8 @@
 import os
+import re
+
 import tweepy as tw
-import pandas as pd
+from requests_oauthlib import OAuth1Session
 
 consumer_key = "FJlEboOOHuPWnP0hiJMcyP5Ue"
 consumer_secret = "AV2c7GZjz5pwNN7XNn0KpcFMJpqZpAqClJbT2BMXJ20xRD6qB1"
@@ -26,3 +28,14 @@ tweets = tw.Cursor(api.search,
 # Iterate and print tweets
 for tweet in tweets:
     print(tweet.text)
+
+
+
+def remove_pattern(text, pattern_regex):
+    r = re.findall(pattern_regex, text)
+    for i in r:
+        text = re.sub(i, '', text)
+    
+    return text
+# We are keeping cleaned tweets in a new column called 'tidy_tweets'
+tweets_df['tidy_tweets'] = np.vectorize(remove_pattern)(tweets_df['tweets'], "@[\w]*: | *RT*")
