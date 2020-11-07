e424be0b1a9c8f2f5cd337dae74217c7dbe52d86
insaiyancvk <cvamshik1@gmail.com>
2020-11-07 21:57:41 +0530

    changed the keys

--- fetch.py
+++ fetch.py
@@ -1,18 +1,17 @@
 import os
 import tweepy as tw
-import pandas as pd
+#import pandas as pd
 
-consumer_key = "FJlEboOOHuPWnP0hiJMcyP5Ue"
-consumer_secret = "AV2c7GZjz5pwNN7XNn0KpcFMJpqZpAqClJbT2BMXJ20xRD6qB1"
-access_token = "1177853975921430528-cTDbQAyGeo8nXhegDUK49jcU7nqHHs"
-access_token_secret = "YzN6sre3uWjxuI0mzdkF0mbgJUL7QUK63TgrkbgypP5vo"
+consumer_key = "ydKKAoXFmqq97PZWYPs4wfy5S"
+consumer_secret = "G1tFaa0Gop0kGCYsZzUpx1RLYjvIFv8qwS9H5dreZkgqlfCEnR"
+access_token = "856759533640622080-HhlrgshaitB8gR2TXvrBq60XtNeRnXP"
+access_token_secret = "8GYPov3JIJAJD8o2PvkGtdrXwUy5HewqEOXeTdAwKZP5Q"
 
 auth = tw.OAuthHandler(consumer_key, consumer_secret, callback=None)
 auth.set_access_token(access_token, access_token_secret)
 api = tw.API(auth)
 
-
-search_words = "#wildfires"
+search_words = "byjus"
 date_since = "2018-11-16"
 
 new_search = search_words + " -filter:retweets"
@@ -21,7 +20,7 @@ new_search = search_words + " -filter:retweets"
 tweets = tw.Cursor(api.search,
               q=new_search,
               lang="en",
-              since=date_since).items(5)
+              since=date_since).items(100)
 
 # Iterate and print tweets
 for tweet in tweets:
